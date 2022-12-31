7f5927ce0cbc12734ff7b0d7908ee63d651a830d
colanyanya <colanyanya@163.com>
2023-01-01 04:28:12 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App-main">
+          <div className="App-caption">
+            <h1>生命不息 摸鱼不止  —— <a href={"https://space.bilibili.com/910547"}>佐倉双夜</a></h1>
+          </div>
+
+        </div>
       </header>
     </div>
   );
